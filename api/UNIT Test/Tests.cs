544f25aca1e50355c4fdc19d57d120639f033dd9
Robert Pedersen <128043312+Robes13@users.noreply.github.com>
2025-01-27 15:38:45 +0100

    yeyye
    eyeyey

---
+++ api/UNIT Test/Tests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+
+namespace api.UNIT_Test
+{
+
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void ValidateData()
+        {
+            string username = "hanssuper400";
+            string hashedpassword = "password";
+
+            string email = "email@zbc.dk";
+
+            // Future enum
+            RoleEnum userRole;
+
+            bool isDeleted = false;
+
+            DateTime? deletionTime = null;
+            return UserRepository.ValidateData(username, hashedpassword, email, userRole, isDeleted, deletionTime);
+        }
+        [TestMethod]
+        public void CreateUser()
+        {
+            string username = "hanssuper400";
+            string hashedpassword = "password";
+            string email = "email@zbc.dk";
+
+            // Future enum
+            RoleEnum userRole;
+
+            bool isDeleted = false;
+
+            DateTime? deletionTime = null;
+
+            return UserRepository.CreateUser(username, hashedpassword, email, userRole, isDeleted, deletionTime);
+        }
+        [TestMethod]
+        public void TestThree() 
+        {
+            
+        }
+
+
+        [TestMethod]
+        public void TestFour()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestFive() 
+        {
+
+        }
+    }
+}
