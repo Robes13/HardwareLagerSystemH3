15dc9835aada59100b779c1f348e4a6779444ad8
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2025-01-28 10:12:49 +0100

    Notification & QRCode

--- api/Models/Notification.cs
+++ api/Models/Notification.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api.Models
 {
     public class Notification
     {
-        
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public int userHardwareId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(255)")]
+        [MaxLength(255, ErrorMessage = "Message is longer than 255 characters")]
+        public string message { get; set; } = null!;
     }
 }
